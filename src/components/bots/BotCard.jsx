bae0face0399f6b2967208ecf87ac762bda41801
SirSimon162 <anmolpattnayak2016@gmail.com>
2024-03-30 11:44:07 +0530

    bot list

--- src/components/bots/BotCard.jsx
+++ src/components/bots/BotCard.jsx
@@ -1,61 +1,58 @@
-import {
-  Tooltip,
-  useDisclosure,
-  Modal,
-  ModalContent,
-  Button,
-  Input
-} from "@nextui-org/react";
-import { IoIosAddCircle } from "react-icons/io";
+import { Tooltip, useDisclosure, Modal, ModalContent, ModalHeader, ModalBody } from "@nextui-org/react";
+import { FaRobot } from "react-icons/fa";
+import { FaCircleInfo } from "react-icons/fa6";
+import TextMessage from "../messages/TextMessage";
 
 export default function BotCard() {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
   return (
-    <div className="flex w-full justify-between items-center px-2 py-2 border-[1px] border-gray-700">
-      <div className="flex gap-2 items-center">
-        <img src="https://i.pravatar.cc/150?u=a04258114e29026702d" alt="bot" className="h-[30px] w-[30px] rounded-md"/>
-        <p className="text-lg font-bold text-white">Lorem Ipsum Bot</p>
+    <div className="flex w-full justify-between items-center px-2 py-2 border-[1px] border-gray-700 cursor-pointer hover:bg-gray-900" onClick={onOpen}>
+      <div className="flex gap-3 items-center">
+        <p className="text-xl text-white">
+          <FaRobot />
+        </p>
+        <div className="flex flex-col items-start">
+
+        <p className="text-md font-bold text-white">Lorem Ipsum Bot</p>
+        <p className="text-sm font-bold text-gray-400">@loremipsum</p>
+        </div>
       </div>
-      <Tooltip content="Add to Chat" className="cursor-pointer">
+      <Tooltip content="Know More" className="cursor-pointer">
         <button
           onClick={onOpen}
           className="bg-transparent text-white text-xl w-fit p-0"
         >
-          <IoIosAddCircle />
+          <FaCircleInfo />
         </button>
       </Tooltip>
       <Modal
-            isOpen={isOpen}
-            placement={"center"}
-            onOpenChange={onOpenChange}
-            className="rounded-sm bg-gray-900 pt-4 pb-8 px-8"
-          >
-            <ModalContent className="">
-              {(onClose) => (
-                <>
-                  <p className="mb-2 text-white text-lg font-bold">
-                    Add To:
-                  </p>
-                  <div className="flex items-center justify-center">
-                    <Input
-                      type="text"
-                      variant={"faded"}
-                      placeholder="Chat Room ID"
-                      radius="none"
-                      className="p-0 outline-none focus:outline-none border-none focus:border-none text-black"
-                    />
-                    <Button
-                      color="primary"
-                      onPress={onClose}
-                      className="rounded-l-none rounded-r-sm text-white"
-                    >
-                      Submit
-                    </Button>
-                  </div>
-                </>
-              )}
-            </ModalContent>
-          </Modal>
+        isOpen={isOpen}
+        placement={"center"}
+        onOpenChange={onOpenChange}
+        className="rounded-sm bg-gray-900 pt-4 pb-8 px-8"
+      >
+        <ModalContent className="">
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col">
+                <p className="text-xl font-bold text-white">Lorem Ipsum Bot</p>
+                <p className="text-md font-regular text-gray-400">@loremipsum</p>
+              </ModalHeader>
+              <ModalBody className="text-white">
+                <p> 
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Nullam pulvinar risus non risus hendrerit venenatis.
+                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
+                </p>
+                <div className="bg-slate-800 pt-2 pb-4 px-4 rounded-md">
+                  <p className="font-bold text-lg mb-4">Example:</p>
+                  <TextMessage message={"Mumbaiche havamana kasa aahe?"} name={"User"} time={"12:00"}/>
+                </div>
+              </ModalBody>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
     </div>
   );
 }
